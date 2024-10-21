f62737f225f2e828fb0f2bbeca6661156e7afca1
Khlood Mahmoud <khloodm390@gmail.com>
2024-10-21 23:59:14 +0300

    Upload Navbar, Cart, CartContext, CourseCard, and App.js for review

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Usection } from '../../components/Usection'
 import { Mainsection } from '../../components/Mainsection'
 import { Courses } from '../../components/Courses';
-import { CourseCard } from '../../components/CourseCard';
+import { CourseCard } from '../../components/CartContext';
 import { Chooseus } from '../../components/Chooseus';
 import { Footer } from '../../components/Footer';
 
