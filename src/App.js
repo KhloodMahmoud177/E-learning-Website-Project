7ff75fe62718d57924c67d73726e03653f33f685
Khlood Mahmoud <khloodm390@gmail.com>
2024-10-21 20:45:52 +0300

    Resolved merge conflicts and updated App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavbarNew } from "./components/Navbar";
-<<<<<<< HEAD
-=======
-// import Button from 'react-bootstrap/Button';
->>>>>>> e01a430b9d2e4d2377f7661ad9c8c400f790ca01
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
+import { BrowserRouter} as Router, Routes, Route } from "react-router-dom";
+import {Home} from "./pages/home/Home";
+import {Login} from "./pages/login/Login";
 import "remixicon/fonts/remixicon.css";
-import SignUp from "./pages/signup/Signup";
-import Dashboard from "./pages/dashboard/Dashboard";
-import AuthProvider from "./statestore/Authcontext";
-<<<<<<< HEAD
-import Cart from "./pages/cart/Cart";
+import {SignUp} from "./pages/signup/Signup";
+import {Dashboard} from "./pages/dashboard/Dashboard";
+import {AuthProvider} from "./statestore/Authcontext";
+import {CoursesPage} from "./pages/coursespage/CoursesPage";
+import {CourseDetailPage} from "./pages/coursedetailpage/CourseDetailPage";
+import {Cart }from "./pages/cart/Cart";
 import { CartProvider } from "./statestore/CartContext"; // Import CartProvider
 
 function App() {
@@ -29,31 +26,12 @@ function App() {
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/signup" element={<SignUp />} />
               <Route path="/cart" element={<Cart />} />
+              <Route path="/courses" element={<CoursesPage />} />
+              <Route path="/courses/:id" element={<CourseDetailPage />} />
             </Routes>
           </Router>
         </AuthProvider>
       </CartProvider>
-=======
-import CoursesPage from "./pages/coursespage/CoursesPage";
-import CourseDetailPage from "./pages/coursedetailpage/CourseDetailPage";
-function App() {
-  return (
-    <div className="App">
-      <AuthProvider>
-        <Router>
-          <NavbarNew />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/courses" element={<CoursesPage />} />
-            <Route path="/courses/:id" element={<CourseDetailPage />} />
-          </Routes>
-        </Router>
-      </AuthProvider>
->>>>>>> e01a430b9d2e4d2377f7661ad9c8c400f790ca01
     </div>
   );
 }
